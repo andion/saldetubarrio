6beb1f66c155a8835fa207bb81aab57f97f82bb5
Lucas Andión <andion@gmail.com>
2011-10-17 00:13:04 -0500

    Empty rails app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Saldetubarrio::Application.config.secret_token = '4ec1bd446276e16f669f93c145dae4fa47740c3427549694b0606abe87c9b1d06d21c557af30b68ba884656e63fd5159a16e74d5af8e68c413db5b3a8faf293d'
